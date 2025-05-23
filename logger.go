476acef18857c53ddbbb785a107ed88812dbce9f
asaldivar <augustosaldivar@assertsolutions.com>
2025-05-23 09:59:40 -0300

    gofmt

--- logger.go
+++ logger.go
@@ -1,61 +1,61 @@
 package gologger
 
 import (
-    "log"
-    "os"
-    "time"
+	"log"
+	"os"
+	"time"
 )
 
 const (
-    DebugLevel string = "debug"
-    InfoLevel  string = "info"
-    WarnLevel  string = "warn"
-    ErrorLevel string = "error"
+	DebugLevel string = "debug"
+	InfoLevel  string = "info"
+	WarnLevel  string = "warn"
+	ErrorLevel string = "error"
 )
 
-func init(){
+func init() {
 
-    time.Local = time.UTC // utc
+	time.Local = time.UTC // utc
 
-    var loggerColor string = os.Getenv("LOGGER_COLOR")
-    var loggerLevel string = os.Getenv("LOGGER_LEVEL")
+	var loggerColor string = os.Getenv("LOGGER_COLOR")
+	var loggerLevel string = os.Getenv("LOGGER_LEVEL")
 
-    if(loggerLevel != DebugLevel && loggerLevel != WarnLevel && loggerLevel != ErrorLevel){
-        loggerLevel = "info"
-    }
+	if loggerLevel != DebugLevel && loggerLevel != WarnLevel && loggerLevel != ErrorLevel {
+		loggerLevel = "info"
+	}
 
-    if(loggerColor == "true"){
-        switch loggerLevel{
-            case DebugLevel:
-                Debug = func(str string){log.Println("\033[34mDEBUG\033[0m "+str)}
-                fallthrough
-            case InfoLevel:
-                Info = func(str string){log.Println("\033[32mINFO\033[0m  "+str)}
-                fallthrough
-            case WarnLevel:
-                Warn = func(str string){log.Println("\033[33mWARN\033[0m  "+str)}
-                fallthrough
-            case ErrorLevel:
-                Error = func(str string){log.Println("\033[31mERROR\033[0m "+str)}
-        }
-    }else{
-        switch loggerLevel{
-            case DebugLevel:
-                Debug = func(str string){log.Println("DEBUG "+str)}
-                fallthrough
-            case InfoLevel:
-                Info = func(str string){log.Println("INFO  "+str)}
-                fallthrough
-            case WarnLevel:
-                Warn = func(str string){log.Println("WARN  "+str)}
-                fallthrough
-            case ErrorLevel:
-                Error = func(str string){log.Println("ERROR "+str)}
-        }
-    }
+	if loggerColor == "true" {
+		switch loggerLevel {
+		case DebugLevel:
+			Debug = func(str string) { log.Println("\033[34mDEBUG\033[0m " + str) }
+			fallthrough
+		case InfoLevel:
+			Info = func(str string) { log.Println("\033[32mINFO\033[0m  " + str) }
+			fallthrough
+		case WarnLevel:
+			Warn = func(str string) { log.Println("\033[33mWARN\033[0m  " + str) }
+			fallthrough
+		case ErrorLevel:
+			Error = func(str string) { log.Println("\033[31mERROR\033[0m " + str) }
+		}
+	} else {
+		switch loggerLevel {
+		case DebugLevel:
+			Debug = func(str string) { log.Println("DEBUG " + str) }
+			fallthrough
+		case InfoLevel:
+			Info = func(str string) { log.Println("INFO  " + str) }
+			fallthrough
+		case WarnLevel:
+			Warn = func(str string) { log.Println("WARN  " + str) }
+			fallthrough
+		case ErrorLevel:
+			Error = func(str string) { log.Println("ERROR " + str) }
+		}
+	}
 }
 
-var Debug = func(str string){}
-var Info = func(str string){}
-var Warn = func(str string){}
-var Error = func(str string){}
+var Debug = func(str string) {}
+var Info = func(str string) {}
+var Warn = func(str string) {}
+var Error = func(str string) {}
--- logger_test.go
+++ logger_test.go
@@ -2,7 +2,7 @@ package gologger
 
 import "testing"
 
-func TestDebug(t *testing.T){
+func TestDebug(t *testing.T) {
 	Debug("hola")
 	Info("hola")
 	Warn("hola")
