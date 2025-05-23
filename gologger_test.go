6d102a2bf90bfc273e560b38ec5673202eb1451c
asaldivar <augustosaldivar@assertsolutions.com>
2025-05-23 10:14:16 -0300

    name_change

--- gologger_test.go
+++ gologger_test.go
@@ -1,10 +1,17 @@
 package gologger
 
-import "testing"
+import (
+	"fmt"
+	"log"
+	"testing"
+	"time"
+)
 
 func TestDebug(t *testing.T) {
 	Debug("hola")
 	Info("hola")
 	Warn("hola")
 	Error("hola")
+	log.Println("utc")
+	fmt.Println(time.Now().Format(time.RFC3339))
 }
